b5ca70395092c99d53ee7c0690d2dac2bd9a3f4b
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-11-23 20:06:18 +0800

    register html
    for data of user/quiz taker

---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>Register</title></head>
+<body>
+    <form method="post">
+        <h2>Register</h2>
+        <input type="text" name="username" required><br>
+        <br>
+        <input type="password" name="password" required><br>
+        <br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
