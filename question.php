1d7bedf419fbf961e8597c93a97d0025d0d05f06
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-11-23 20:47:58 +0800

    10 points

--- question.php
+++ question.php
@@ -20,7 +20,44 @@ $questions = [
         "question" => "Which function is used to include a file in PHP?",
         "options" => json_encode(["include()", "require()", "import()", "load()"]),
         "answer" => 0
+    ],
+    [
+        "question" => "What is the equivalent of (print in pyhton) in PHP?",
+        "options" => json_encode(["print", "echo", "Console.wwriteline", "Show"]),
+        "answer" => 1
+    ],
+    [
+        "question" => "What are the symbol of single line comment for PHP?",
+        "options" => json_encode(["com.(),<--", "comment,-->", "//,#", "<--,-->"]),
+        "answer" => 2
+    ],
+    [
+        "question" => "What is the symbol of Multi line comment for PHP?",
+        "options" => json_encode(["com.()", "/*", "//", "<---->"]),
+        "answer" => 1
+    ],
+    [
+        "question" => "Can PHP be use in Offline?",
+        "options" => json_encode(["True","False"]),
+        "answer" => 0
+    ],
+    [
+        "question" => "Is PHP case Sensitive?",
+        "options" => json_encode(["True","False"]),
+        "answer" => 0
+    ],
+    [
+        "question" => "what does this code Show echo. I Love PHP?",
+        "options" => json_encode(["echo. I Love PHP", "I Love PHP", "echo ILovePHP", "ILovePHP"]),
+        "answer" => 1
+    ],
+    [
+        "question" => "What is the best PHP Syntax?",
+        "options" => json_encode(["<?PHP?>", "<php?>", "<?php?>", "<?php>"]),
+        "answer" => 2
     ]
+
+    
 ];
 
 foreach ($questions as $q) {
