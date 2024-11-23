3c72b3217b14913b5e5f6e870ef892c1f64e0350
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-11-23 22:14:31 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,33 @@
-<?php
+
+<!DOCTYPE html>
+<html>
+<head><title>Register</title></head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<style>
+        .container
+        {
+            background-color: white;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 80%;
+            max-width: 225px;
+        }
+</style>
+<body>
+    <div class="container">
+    <form method="post">
+        <h2>Register</h2>
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username" required><br>
+        <br>
+        <label for="passwords">Password</label><br>
+        <input type="password" name="passwords" id="passwords" required><br>
+        <br>
+        <button type="submit">Register</button>
+    </form>
+    </div>
+    <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = password_hash($_POST['passwords'], PASSWORD_BCRYPT);
@@ -28,42 +57,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn->close();
 }
 ?>
-
-
-<!DOCTYPE html>
-<html>
-<head><title>Register</title></head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<style>
-    body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f8f9fa;
-        }
-        .container
-        {
-            background-color: white;
-            border-radius: 10px;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 80%;
-            max-width: 225px;
-        }
-</style>
-<body>
-    <div class="container">
-    <form method="post">
-        <h2>Register</h2>
-        <label for="username">Username</label><br>
-        <input type="text" name="username" id="username" required><br>
-        <br>
-        <label for="passwords">Password</label><br>
-        <input type="password" name="passwords" id="passwords" required><br>
-        <br>
-        <button type="submit">Register</button>
-    </form>
-    </div>
 </body>
 </html>
